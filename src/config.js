31c446d89fa285ee51ada21f539d09460905d6cc
rboswellj <rboswellj@gmail.com>
2022-03-31 16:12:03 -0400

    fixing variables for heroku config

--- src/config.js
+++ src/config.js
@@ -2,10 +2,11 @@
 
 let config = {
     dbPort: 27017,
-    dbUrl: "mongodb://localhost:27017/dungeon-diver",
+    localDbUrl: "mongodb://localhost:27017/dungeon-diver",
+    dbUrl: process.env.MONGODB_URI,
     apiServerPort: `5000`,
     appServerPort: `8080`,
     jsonFile: 'assets/dungeonDiverData.json'
-  };
-  
+    };
+
   module.exports = config;
